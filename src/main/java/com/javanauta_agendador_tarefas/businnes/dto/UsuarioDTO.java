81217c23d9f3fbbad7bc66d1e3faf41bdd201a84
brunorafaeldev <brunorafaellira@hotmail.com>
2026-08-26 14:50:32 -0300

    Adicionadas as configurações de segurança

---
+++ src/main/java/com/javanauta_agendador_tarefas/businnes/dto/UsuarioDTO.java
@@ -0,0 +1,21 @@
+package com.javanauta_agendador_tarefas.businnes.dto;
+
+
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UsuarioDTO {
+
+
+        private String email;
+        private String senha;
+
+
+
+}
